846026f51f906d2a9e6aa24dbb042ceb2d59bbd8
Chris Joyce <CJoyce@jhacorp.com>
2021-10-01 18:10:33 -0500

    Lesson 10

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const routes: Routes = [
+  {path: "", redirectTo: "/Login", pathMatch: "full"},
+  {path: "courses", 
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule) },
+  {path: "Login", component: LoginComponent},
+  {path: "about", component: AboutComponent},
+  {path: "**", component: PageNotFoundComponent}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  providers: [
+
+  ]
+})
+export class AppRoutingModule {
+
+
+}
---
+++ src/app/courses/courses-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CourseComponent } from './course/course.component';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "courseUrl", component: CourseComponent}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule],
+  providers: [
+  ]
+})
+export class CoursesRoutingModule {
+
+
+}
